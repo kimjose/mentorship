a2343c48e08cbdbcf793b8f344341e6e224d91fc
Kimani Joseph <kimjose693@gmail.com>
2023-03-13 09:23:02 +0300

    minor

--- web/home.php
+++ web/home.php
@@ -1,56 +1,30 @@
-
 <!-- Info boxes -->
-  <div class="row">
-    <div class="col-12 col-sm-6 col-md-3">
-      <div class="info-box">
-        <span class="info-box-icon bg-info elevation-1"><i class="fas fa-users"></i></span>
+<div class="row">
+  <div class="col-12 col-sm-6 col-md-3">
 
-        <div class="info-box-content">
-          <span class="info-box-text">Total Subscribers</span>
-          <span class="info-box-number">
-          </span>
-        </div>
-        <!-- /.info-box-content -->
-      </div>
-      <!-- /.info-box -->
-    </div>
-    <!-- /.col -->
-    <div class="col-12 col-sm-6 col-md-3">
-      <div class="info-box mb-3">
-        <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-poll-h"></i></span>
-
-        <div class="info-box-content">
-          <span class="info-box-text">Total Checklist</span>
-          <span class="info-box-number">
-          </span>
-        </div>
-        <!-- /.info-box-content -->
-      </div>
-      <!-- /.info-box -->
-    </div>
-    <!-- /.col -->
+    <!-- /.info-box -->
   </div>
+  <!-- /.col -->
+  <div class="col-12 col-sm-6 col-md-3">
+    <div class="info-box mb-3">
+      <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-poll-h"></i></span>
 
-  <div class="col-12">
+      <div class="info-box-content">
+        <span class="info-box-text">Total Checklist</span>
+        <span class="info-box-number">
+        </span>
+      </div>
+      <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+  </div>
+  <!-- /.col -->
+</div>
+
+<div class="row">
+  <div class="col-md-3 col-sm-6 info-box p-2">
     <div class="card">
-      <div class="card-body">
-        Welcome <?php echo $currUser->first_name . ' ' . $currUser->last_name ?>!
-      </div>
+      
     </div>
   </div>
-  <div class="row">
-    <div class="col-12 col-sm-6 col-md-3">
-      <div class="info-box">
-        <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-poll-h"></i></span>
-
-        <div class="info-box-content">
-          <span class="info-box-text">Total Checklist Done</span>
-          <span class="info-box-number">
-          </span>
-        </div>
-        <!-- /.info-box-content -->
-      </div>
-      <!-- /.info-box -->
-    </div>
-    <!-- /.col -->
-  </div>
+</div>
